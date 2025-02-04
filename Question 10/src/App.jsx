98755f8d019f6a388c92761fd6e6458bfb6f1de7
Tushar Batra <tusharbatra08@gmail.com>
2025-02-04 11:07:54 +0530

    Handled Debounce search question

---
+++ Question 10/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import DebounceSearch from './DebounceSearch'
+
+function App() {
+
+  return (
+    <>
+      <DebounceSearch />
+    </>
+  )
+}
+
+export default App
---
+++ Question 10/src/DebounceSearch.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+
+const DebounceSearch = () => {
+  const [users, setUsers] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [searchedUsers, setSearchedUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      let data = await fetch("https://dummyjson.com/users");
+      let json = await data.json();
+      setUsers(json.users);
+      setSearchedUsers(json.users); // Initialize search results with all users
+    };
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const filteredUsers = users.filter((user) =>
+        user.firstName.toLowerCase().includes(inputValue.toLowerCase())
+      );
+      setSearchedUsers(filteredUsers);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [inputValue, users]);
+
+  return (
+    <div>
+      <input
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Search users..."
+      />
+      {searchedUsers.length > 0 &&
+        searchedUsers.map((user) => <h1 key={user.id}>{user.firstName}</h1>)}
+    </div>
+  );
+};
+
+export default DebounceSearch;
